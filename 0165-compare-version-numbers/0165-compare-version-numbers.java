b44fee689a4406a20cc31908f14f0084b4bfce74
Doa'a OsamaK <147305995+DoaaOsamaK@users.noreply.github.com>
2024-05-03 05:40:45 +0300

    Time: 0 ms (100.00%), Space: 41.4 MB (54.87%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i; 
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j;
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+}
